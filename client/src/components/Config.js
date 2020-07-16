6761447df770b3460bb40ca8d048cd5e9879bcef
arthur-ryoo <jene0523@gmail.com>
2020-07-15 22:38:35 -0700

    Add api configuration

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_KEY = 'bc9f1d3bea596d9011b4ba28e20dc35e';
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_URL = 'https:Image.tmdb.org/t/p/';
