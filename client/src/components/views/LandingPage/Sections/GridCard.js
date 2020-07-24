0232f9ade8480d0200886b78641494c65608c684
arthur-ryoo <jene0523@gmail.com>
2020-07-23 22:18:38 -0700

    Delete props movie id

--- client/src/components/views/LandingPage/Sections/GridCard.js
+++ client/src/components/views/LandingPage/Sections/GridCard.js
@@ -3,7 +3,17 @@ import { Col } from 'antd';
 
 function GridCard(props) {
   if (props.actor) {
-    return <div></div>;
+    return (
+      <Col lg={6} md={8} xs={24}>
+        <div style={{ position: 'relative' }}>
+          <img
+            style={{ width: '100%', height: '320px' }}
+            alt="img"
+            src={props.image}
+          ></img>
+        </div>
+      </Col>
+    );
   } else {
     return (
       <Col lg={6} md={8} xs={24}>
