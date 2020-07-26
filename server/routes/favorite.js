d7c681ee1c9ac0c4e12072cb296d9759f472324c
arthur-ryoo <jene0523@gmail.com>
2020-07-25 23:41:28 -0700

    Create controller for favorite button

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -10,8 +10,21 @@ const { auth } = require('../middleware/auth');
 
 router.post('/favoriteNumber', auth, (req, res) => {
   Favorite.find({ movieId: req.body.movieId }).exec((err, favorite) => {
-    if (eff) return res.status(400).send(err);
-    res.status(200).json({ success: true, FavoriteNumber: favorite.length });
+    if (err) return res.status(400).send(err);
+    res.status(200).json({ success: true, favoriteNumber: favorite.length });
+  });
+});
+
+router.post('/favorited', auth, (req, res) => {
+  Favorite.find({ movieId: req.body.movieId, userFrom: req.body.userFrom });
+  exec((err, favorite) => {
+    if (err) return res.status(400).send(err);
+
+    let result = false;
+    if (favorite.length !== 0) {
+      result = true;
+    }
+    res.status(200).json({ success: true, favorited: result });
   });
 });
 
