a0c13dadee3e3615a3cfe7c3c8ff50da13521d5c
arthur-ryoo <jene0523@gmail.com>
2020-07-26 22:20:32 -0700

    Create on click function for favorite button

--- client/src/components/views/MovieDetailPage/Sections/Favorite.js
+++ client/src/components/views/MovieDetailPage/Sections/Favorite.js
@@ -5,15 +5,15 @@ function Favorite(props) {
   const [FavoriteNumber, setFavoriteNumber] = useState(0);
   const [Favorited, setFavorited] = useState(false);
 
-  useEffect(() => {
-    const variable = {
-      userFrom: props.userFrom,
-      movieId: props.movieId,
-      movieTitle: props.movieInfo.original_title,
-      movieImage: props.movieInfo.backdrop_path,
-      movieRunTime: props.movieInfo.runtime,
-    };
+  const variable = {
+    userFrom: props.userFrom,
+    movieId: props.movieId,
+    movieTitle: props.movieInfo.original_title,
+    movieImage: props.movieInfo.backdrop_path,
+    movieRunTime: props.movieInfo.runtime,
+  };
 
+  useEffect(() => {
     axios.post('./api/favorite/favoriteNumber', variable).then((response) => {
       if (response.data.success) {
         setFavoriteNumber(response.data.favoriteNumber);
@@ -31,9 +31,33 @@ function Favorite(props) {
     });
   }, []);
 
+  const onClickFavorite = () => {
+    if (Favorited) {
+      axios
+        .post('/api/favorite/removeFromFavorite', variable)
+        .then((response) => {
+          if (response.data.success) {
+            setFavoriteNumber(FavoriteNumber - 1);
+            setFavorited(!Favorited);
+          } else {
+            alert('Failed to remove from favorite');
+          }
+        });
+    } else {
+      axios.post('/api/favorite/addToFavorite', variable).then((response) => {
+        if (response.data.success) {
+          setFavoriteNumber(FavoriteNumber + 1);
+          setFavorited(!Favorited);
+        } else {
+          alert('Failed to add to Favorites');
+        }
+      });
+    }
+  };
+
   return (
     <div>
-      <button>
+      <button onClick={onClickFavorite}>
         {Favorited ? ' remove from Favorite ' : ' Add to Favorite'}{' '}
         {FavoriteNumber}
       </button>
