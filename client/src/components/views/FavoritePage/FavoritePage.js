34801cc7997b735b9871608e168c7abb425b9487
arthur-ryoo <jene0523@gmail.com>
2020-07-31 20:13:20 -0700

    Add table for favorite movie list

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import './FavoritePage.css';
 
 function FavoritePage() {
-  return <div></div>;
+  return (
+    <div style={{ width: '85%', margin: '3rem auto' }}>
+      <h3>Favorite Movie List</h3>
+      <hr />
+
+      <table>
+        <thead>
+          <tr>
+            <th>Movie Title</th>
+            <th>Movie Run Time</th>
+            <th>Remove from Favorites</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
 }
 
 export default FavoritePage;
