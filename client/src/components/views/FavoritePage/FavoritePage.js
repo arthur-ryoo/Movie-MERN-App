f26c35dd58daff8399d3f94dc98ca1b3ff54cd8c
arthur-ryoo <jene0523@gmail.com>
2020-08-01 22:09:57 -0700

    Add favorite api and table to display movie data

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './FavoritePage.css';
+import Axios from 'axios';
 
 function FavoritePage() {
+  const variables = { userFrom: localStorage.getItem('userId') };
+
+  const [FavoritedMovies, setFavoritedMovies] = useState([]);
+
+  useEffect(() => {
+    Axios.post('./api/favorite/getFavoritedMovie', variables).then(
+      (response) => {
+        if (response.data.success) {
+          setFavoritedMovies(response.data.favorites);
+        } else {
+          alert('Failed to get favorited movies');
+        }
+      }
+    );
+  }, []);
+
+  const renderTableBody = FavoritedMovies.map((movie, index) => {
+    console.log(movie.movieTitle);
+    return (
+      <tr>
+        <td>{movie.movieTitle}</td>
+
+        <td>{movie.movieRunTime} mins</td>
+        <td>
+          <button>Remove from the Favorites</button>
+        </td>
+      </tr>
+    );
+  });
+
   return (
     <div style={{ width: '85%', margin: '3rem auto' }}>
       <h3>Favorite Movie List</h3>
@@ -15,6 +46,7 @@ function FavoritePage() {
             <th>Remove from Favorites</th>
           </tr>
         </thead>
+        <tbody>{renderTableBody}</tbody>
       </table>
     </div>
   );
